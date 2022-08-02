656194931ff5d50a39e1d996a9de4ad0e9be304c
Michael Mowry <michael.mowry@stellence.com>
2022-08-02 08:56:16 -0700

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Transition } from "@headlessui/react";
+import { Link } from "react-scroll";
+
+function Navbar() {
+    return {
+        < div >
+        <nav className='shadow-sm fixed w-full z-10' >
+            <div className='w-full'>
+                <div className='flex items-center h-20 w-full'></div>
+            </div>
+        </nav>
+        </div >
+    }
+}
+
+export default Navbar
